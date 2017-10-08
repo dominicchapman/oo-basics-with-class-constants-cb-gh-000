c7d5dd4925e0aba71c340f8c5d5d049fc4c736a0
Dominic Chapman <dominicchapman@me.com>
2017-10-08 13:44:45 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,18 +6,15 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    def brand_exists?(brand)
+      BRANDS.any? { |existing_brand| existing_brand == brand }
+    end
+    brand_exists ? (BRANDS << brand) : return
+    
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
-  end
-
-  def brand=(brand)
-    @brand = brand
-    def brand_exists?(brand)
-      BRANDS.any? { |existing_brand| existing_brand == brand }
-    end
-    brand_exists ? (BRANDS << brand) : return
   end
 end
